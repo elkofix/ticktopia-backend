4510f990317110440d6b1dddf4f883e381704cce
elkofix <holasoygermamwow@gmail.com>
2025-05-30 22:29:27 -0500

    feat: adding domain

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     })
   )
   app.enableCors({
-    origin: ['http://localhost:8080'],
+    origin: ['http://localhost:8080', "https://d16p2eueveuhxz.cloudfront.net"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
